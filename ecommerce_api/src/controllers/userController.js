b9394a72d53dd4170b6b37707b85fb69d5c8614f
Virali Singh <viralisingh1998@gmail.com>
2025-03-11 19:08:20 +0530

    login api created and learn about the bcrypt password hashing

--- ecommerce_api/src/controllers/userController.js
+++ ecommerce_api/src/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import bcrypt from "bcrypt";
 
 const registerUser = async (req, res, next) => {
     try {
@@ -10,12 +10,14 @@ const registerUser = async (req, res, next) => {
             res.status(400);
             throw new Error('User already exists');
         } else {
+
+            const hashPassword = await bcrypt.hash(password, 14);
             const user = await User.create({
                 name,
                 email,
                 age,
                 address,
-                password
+                password: hashPassword
             });
 
             res.status(201).json({
@@ -32,7 +34,30 @@ const registerUser = async (req, res, next) => {
 
 
 
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error('Invalid email or password');
+        }
+        // if (password !== user.password) {
+        //     throw new Error('Invalid email or password');
+        // }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            throw new Error('Invalid email or password');
+        }
+        return res.json({ user });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
 
 export {
-    registerUser
+    registerUser, loginUser
 };
